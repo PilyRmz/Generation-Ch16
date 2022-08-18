30c97df3db263d1140ce32cab71c8ca6df871dbd
PilyRmz <100257025+PilyRmz@users.noreply.github.com>
2022-08-18 11:34:45 -0500

    Add files via upload
    git commit -m "variables java"
    
    git commit -m "string java"

---
+++ JAVA GENERATION/Prueba1/src/test/java/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo Java"
+        String nombre = "Pily";
+        System.out.println("saludos desde Java a: " + nombre);
+    }
+}
+
