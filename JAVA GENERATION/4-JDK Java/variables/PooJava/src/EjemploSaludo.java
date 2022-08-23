fbd8865ac3bc1fc3cc19719ed7690c8805158f91
PilyRmz <100257025+PilyRmz@users.noreply.github.com>
2022-08-23 16:17:22 -0500

    Add files via upload
    git commit -m " PooJava"

---
+++ JAVA GENERATION/4-JDK Java/variables/PooJava/src/EjemploSaludo.java
@@ -0,0 +1,15 @@
+public class EjemploSaludo {
+    public static void main(String[] args) {
+
+        Saludo objSaludo;
+        objSaludo = new Saludo();
+        objSaludo.saludar();
+
+        Saludo objSaludo0 = new Saludo()
+
+        System.out.println(objSaludo.saludar0());
+        System.out.println("este es otro objeto: ->" + objSaludo.saludar0());
+
+        }
+    }
+}
---
+++ JAVA GENERATION/4-JDK Java/variables/PooJava/src/Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo{
+        void saludar() {
+            System.out.println("Hola Java OOP");
+        }
+        String saludar0(){
+            String textoS = "Hola mundo Java oop";
+            return textoS;
+    }
+}
